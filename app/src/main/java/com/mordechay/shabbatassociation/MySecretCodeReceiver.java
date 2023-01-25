ab92280b6602fe1f6e9b306108ce74d7c9cf7274
Mordechay <m8572998@gmail.com>
2023-01-25 23:05:14 +0200

    add secret code to open main activity

---
+++ app/src/main/java/com/mordechay/shabbatassociation/MySecretCodeReceiver.java
@@ -0,0 +1,23 @@
+package com.mordechay.shabbatassociation;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public final class MySecretCodeReceiver extends BroadcastReceiver {
+    public static final String SECRET_CODE_ACTION = "android.provider.Telephony.SECRET_CODE";
+    public static final String TAG = "secretCodeReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction() == null){
+            Log.e(TAG, "Null action");
+        }else if (intent.getAction().equals(SECRET_CODE_ACTION)) {
+            Log.i(TAG, "Receive secret code intent");
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
