f9984bad0ae8766da5dbf159b10b24c4e4bee712
Mordechay <m8572998@gmail.com>
2023-01-31 20:19:36 +0200

    add import file, export file, and view file

--- app/src/main/java/com/mordechay/shabbatassociation/mangerActivity.java
+++ app/src/main/java/com/mordechay/shabbatassociation/mangerActivity.java
@@ -1,40 +1,73 @@
 package com.mordechay.shabbatassociation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class mangerActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnEnableApp;
+    private Button btnImportFile;
+    private Button btnExportFile;
+    private TextView txtViewFile;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manger);
 
-btnEnableApp = findViewById(R.id.manger_btn_enable_app);
-btnEnableApp.setOnClickListener(this);
+        btnEnableApp = findViewById(R.id.manger_btn_enable_app);
+        btnEnableApp.setOnClickListener(this);
+        btnImportFile = findViewById(R.id.manger_btn_import_file);
+        btnImportFile.setOnClickListener(this);
+        btnExportFile = findViewById(R.id.manger_btn_export_file);
+        btnExportFile.setOnClickListener(this);
+        txtViewFile = findViewById(R.id.manger_txt_file_view);
+        loadingFileView();
+    }
 
+    private void loadingFileView() {
+        String[] txtFile = readFileForEnableApp();
+        if(txtFile != null) {
+            for (String listApp : txtFile) {
+                txtViewFile.setText(txtViewFile.getText() + listApp + "\n");
+            }
+        }else{
+            txtViewFile.setText("לא נמצא קובץ.");
+        }
     }
 
     public void enableAPP(){
-        String[] list = readFile();
+        String[] list = readFileForEnableApp();
         sendCommand(list);
     }
 
-    private String[] readFile() {
+    private String[] readFileForEnableApp() {
         String[] str = null;
         try {
-            FileInputStream fis = new FileInputStream(new File(getFilesDir(),"file.txt"));
+            FileInputStream fis = new FileInputStream(new File(getFilesDir(),Data.FILE_NAME));
             InputStreamReader isr = new InputStreamReader(fis);
             BufferedReader br = new BufferedReader(isr);
             StringBuilder sb = new StringBuilder();
@@ -52,7 +85,7 @@ btnEnableApp.setOnClickListener(this);
     }
 
     private void sendCommand(String[] list) {
-        Process p = null;
+        Process p;
         DataOutputStream os = null;
 
         try {
@@ -62,15 +95,17 @@ btnEnableApp.setOnClickListener(this);
             e.printStackTrace();
         }
 
-        for (int i = 0; i < list.length; i++) {
+        for (String app : list) {
             try {
 
-                os.writeBytes("pm " + "enable " + list[i] + "\n");
+                assert os != null;
+                os.writeBytes("pm " + "enable " + app + "\n");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
         try {
+            assert os != null;
             os.writeBytes("exit\n");
             os.flush();
         } catch (IOException e) {
@@ -80,7 +115,95 @@ btnEnableApp.setOnClickListener(this);
 
     @Override
     public void onClick(View v) {
-        if(v == btnEnableApp)
+        if(v == btnEnableApp) {
             enableAPP();
+        } else if (v == btnImportFile) {
+            importFile();
+        } else if (v == btnExportFile) {
+            exportFile();
+        }
     }
+
+
+
+
+
+    public void importFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("text/*");
+        openActivityForImportFile.launch(intent);
+    }
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    ActivityResultLauncher<Intent> openActivityForImportFile = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    // There are no request codes
+                    Intent data = result.getData();
+                    Uri uri;
+                    if (data != null) {
+                        uri = data.getData();
+                        saveFileToInternalStorage(uri);
+                    }
+                }
+            });
+
+
+    private void saveFileToInternalStorage(Uri fileUri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(fileUri);
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(getFilesDir(), Data.FILE_NAME));
+            byte[] buffer = new byte[1024];
+            int bufferLength;
+            while ((bufferLength = inputStream.read(buffer)) > 0) {
+                fileOutputStream.write(buffer, 0, bufferLength);
+            }
+            fileOutputStream.close();
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        loadingFileView();
+    }
+
+    public void exportFile() {
+        String currentDate = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault()).format(new Date());
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.setType("text/*");
+        intent.putExtra(Intent.EXTRA_TITLE, "list app - " + currentDate + ".mdy");
+        openActivityForExportFile.launch(intent);
+    }
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    ActivityResultLauncher<Intent> openActivityForExportFile = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    // There are no request codes
+                    Intent data = result.getData();
+                    Uri uri;
+                    if (data != null) {
+                        uri = data.getData();
+                        saveFileToExternalStorage(uri);
+                    }
+                }
+            });
+
+    private void saveFileToExternalStorage(Uri fileUri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(Uri.fromFile(new File(getFilesDir(), Data.FILE_NAME)));
+            OutputStream outputStream = getContentResolver().openOutputStream(fileUri);
+            byte[] buffer = new byte[1024];
+            int bufferLength;
+            while ((bufferLength = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, bufferLength);
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
